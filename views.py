08730b5ceefd265b5c56a000c629a7dbb1f8c2a6
Carlota Julbe <113508015+cjulbe@users.noreply.github.com>
2023-05-06 05:55:30 +0200

    Delete views.py

--- views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-#Define that this file is the Blueprint of our application, has different routes inside
-views = Blueprint('views',__name__) #Set up a Blueprint for our Flask application
-
-@views.route('/') #When we call this route it will call the home function
-def home():
-    return render_template("home.html")
-
